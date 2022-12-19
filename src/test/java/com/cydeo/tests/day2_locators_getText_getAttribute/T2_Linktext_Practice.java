65c79c29285afca6b669aac05efdfc21d4142f03
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-19 19:05:38 +0300

    T2_Linktext_Practice created

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_Linktext_Practice.java
@@ -0,0 +1,42 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_Linktext_Practice {
+
+    // TC #2: Back and forth navigation
+    // 1- Open a chrome browser
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //TC #1: Cydeo practice tool verifications
+        // 1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        // 2- Go to: https://practice.cydeo.com
+
+        driver.navigate().to("https://practice.cydeo.com");
+        //driver.get("https://practice.cydeo.com");
+
+
+        // 3- Click to A/B Testing from top of the list.
+            //Thread.sleep(2000);
+            //driver.findElement(By.linkText("A/B Testing")).click();
+
+        WebElement abTestLink = driver.findElement(By.linkText("A/B Testing"));
+        abTestLink.click();
+
+        // 4- Verify title is:
+        //  Expected: No A/B Test,,
+        // 5- Go back to home page by using the .back();
+        //  6- Verify title equals:
+        //  Expected: Practice
+    }
+}
